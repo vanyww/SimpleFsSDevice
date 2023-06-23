d220bb706c5daa2ad1c76289a2d8a5987b347ec6
nxxio <kolyann088@gmail.com>
2023-06-23 21:01:30 +0300

    Add CreateTestData function in main function for initialize test files data

--- Test/main.c
+++ Test/main.c
@@ -1,5 +1,7 @@
 #include "unity_fixture.h"
 
+#include "TestDataBuilder/test_data.h"
+
 static void RunAllTests(void)
 {
    RUN_TEST_GROUP(InitializationTests);
@@ -7,6 +9,7 @@ static void RunAllTests(void)
 
 int main (int argc, char const *argv[])
 {
+   CreateTestData();
    UnityMain (argc, argv, RunAllTests);
    return 0;
 }
