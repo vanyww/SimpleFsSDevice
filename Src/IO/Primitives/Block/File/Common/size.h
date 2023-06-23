6c278647d6afebccd2d3012ac2b88e2518eb7133
nxxio <kolyann088@gmail.com>
2023-06-23 13:55:54 +0300

    Update data types in functions associated with FileAreaTagBlock type

--- Src/IO/Primitives/Block/File/Common/size.h
+++ Src/IO/Primitives/Block/File/Common/size.h
@@ -5,7 +5,7 @@
 #define MAX_FILE_AREA_LENGTH UINT8_MAX
 #define MAX_FILE_SIZE ((MAX_FILE_AREA_LENGTH + 1) * SIZEOF_MEMBER(FileDataBlock, Data))
 
-static inline size_t ComputeFileAreaLength(size_t fileSize)
+static inline uint8_t ComputeFileAreaLength(size_t fileSize)
 {
    return fileSize / SIZEOF_MEMBER(FileDataBlock, Data);
 }
@@ -15,16 +15,16 @@ static inline size_t ComputeTotalFileLength(size_t fileSize)
    return ComputeFileAreaLength(fileSize) + 1;
 }
 
-static inline size_t ComputeFileLastBlockDataSize(uint16_t fileSize)
+static inline uint8_t ComputeFileLastBlockDataSize(size_t fileSize)
 {
    if(fileSize == 0)
       return 0;
 
-   size_t fileSizeRemainder = fileSize % SIZEOF_MEMBER(FileDataBlock, Data);
+   uint8_t fileSizeRemainder = fileSize % SIZEOF_MEMBER(FileDataBlock, Data);
    return (fileSizeRemainder != 0) ? fileSizeRemainder : SIZEOF_MEMBER(FileDataBlock, Data);
 }
 
-static inline size_t ComputeFileSize(size_t fileAreaLength, size_t fileLastBlockDataSize)
+static inline size_t ComputeFileSize(uint8_t fileAreaLength, uint8_t fileLastBlockDataSize)
 {
    return fileAreaLength * SIZEOF_MEMBER(FileDataBlock, Data) + fileLastBlockDataSize;
 }
