44d473c2896ceb1dac12a42490f7c6f446aeebd2
nxxio <kolyann088@gmail.com>
2023-06-26 19:57:11 +0300

    Add RUN_TEST_GROUP macro for transfer test group  in RunAllTests function

--- Test/main.c
+++ Test/main.c
@@ -7,6 +7,7 @@ static void RunAllTests(void)
    RUN_TEST_GROUP(InitializationTests);
    RUN_TEST_GROUP(ReadWrite);
    RUN_TEST_GROUP(CommonSimpleFs);
+   RUN_TEST_GROUP(Transfer);
 }
 
 int main (int argc, char const *argv[])
