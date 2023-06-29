837a1651d9487918086b2c84bb7c6f5358f0a647
nxxio <kolyann088@gmail.com>
2023-06-29 22:45:19 +0300

    Add RUN_TEST_GROUP macro for BadArea test group to RunAllTests function

--- Test/main.c
+++ Test/main.c
@@ -8,6 +8,7 @@ static void RunAllTests(void)
    RUN_TEST_GROUP(ReadWrite);
    RUN_TEST_GROUP(CommonSimpleFs);
    RUN_TEST_GROUP(Transfer);
+   RUN_TEST_GROUP(BadArea);
 }
 
 int main (int argc, char const *argv[])
