614fd32167cb9533846d8c5f2afa017f466484e7
nxxio <kolyann088@gmail.com>
2023-06-29 22:40:20 +0300

    Implement new transfer tests

--- Test/Transfer/tests.c
+++ Test/Transfer/tests.c
@@ -3,6 +3,8 @@
 #include "../Application/application.h"
 #include "../TestDataBuilder/test_data.h"
 #include "../Weak/weak.h"
+#include <stdlib.h>
+#include <time.h>
 
 
 TEST_GROUP(Transfer);
@@ -79,8 +81,140 @@ TEST(Transfer, TransferWithRewriteFile)
    TEST_ASSERT_EQUAL(sizeOfReadSecondFile, secondFileDataSize);
 }
 
+TEST(Transfer, TransferWithBadBlocks)
+{
+   ShowLog(false);
+
+   uint16_t firstFileDataSize = GetTestFileDataSize(FILE_WITH_FULLY_FILLED_DATA_BLOCKS);
+   char firstFileData[firstFileDataSize];
+   CopyTestFileImageData(firstFileData, FILE_WITH_FULLY_FILLED_DATA_BLOCKS);
+
+   uint16_t secondFileDataSize = GetTestFileDataSize(FILE_WITH_INCOMPLETE_FILLED_LAST_DATA_BLOCK);
+   char secondFileData[secondFileDataSize];
+   CopyTestFileImageData(secondFileData, FILE_WITH_INCOMPLETE_FILLED_LAST_DATA_BLOCK);
+
+   uint16_t badBlockNumbers[] = {2, 6, 10, 14, 18, 22};
+   SetGlobalBadBlocksNumbersArrayPtr(badBlockNumbers, 1);
+   SetGlobalBadBlocksNumbersArraySize(sizeof(badBlockNumbers), 1);
+   SetGlobalBadBlocksNumbersArrayPtr(badBlockNumbers, 0);
+   SetGlobalBadBlocksNumbersArraySize(sizeof(badBlockNumbers), 0);
+
+   CREATE_SIMPLE_FS_APPLICATION(200, this);
+
+   SIMPLE_FS_DISPOSE_HANDLE_CLEANUP_ATTRIBUTE SDEVICE_HANDLE(SimpleFs) *handle =
+         SDEVICE_CREATE_HANDLE(SimpleFs)(&INIT(this), NULL, 0, NULL);
+   SetAssertFailhandle(handle);
+
+   SimpleFsSDeviceWriteFile(handle, 0, firstFileData, firstFileDataSize);
+   SimpleFsSDeviceWriteFile(handle, 1, secondFileData, secondFileDataSize);
+
+   for (uint16_t i = 0; i < 10000; i++)
+   {
+      SimpleFsSDeviceForceHistoryDeletion(handle);
+   }
+
+   size_t sizeOfFirstFile = SimpleFsSDeviceReadFile(handle, 0, firstFileData, 14);
+   size_t sizeOfSecondFile = SimpleFsSDeviceReadFile(handle, 1, secondFileData, 17);
+
+   TEST_ASSERT_EQUAL(14, sizeOfFirstFile);
+   TEST_ASSERT_EQUAL(17, sizeOfSecondFile);
+}
+
+TEST(Transfer, TransferWithRewriteFileAndBadBlocks)
+{
+   ShowLog(false);
+
+   uint16_t firstFileDataSize = GetTestFileDataSize(FILE_WITH_FULLY_FILLED_DATA_BLOCKS);
+   char firstFileData[firstFileDataSize];
+   CopyTestFileData(firstFileData, FILE_WITH_FULLY_FILLED_DATA_BLOCKS);
+
+   uint16_t secondFileDataSize = GetTestFileDataSize(FILE_WITH_INCOMPLETE_FILLED_LAST_DATA_BLOCK);
+   char secondFileData[secondFileDataSize];
+   CopyTestFileData(secondFileData, FILE_WITH_INCOMPLETE_FILLED_LAST_DATA_BLOCK);
+
+   uint16_t badBlockNumbers[] = {2, 6, 10, 14, 18, 22};
+   SetGlobalBadBlocksNumbersArrayPtr(badBlockNumbers, 1);
+   SetGlobalBadBlocksNumbersArraySize(sizeof(badBlockNumbers), 1);
+   SetGlobalBadBlocksNumbersArrayPtr(badBlockNumbers, 0);
+   SetGlobalBadBlocksNumbersArraySize(sizeof(badBlockNumbers), 0);
+
+   CREATE_SIMPLE_FS_APPLICATION(200, this);
+
+   SIMPLE_FS_DISPOSE_HANDLE_CLEANUP_ATTRIBUTE SDEVICE_HANDLE(SimpleFs) *handle =
+         SDEVICE_CREATE_HANDLE(SimpleFs)(&INIT(this), NULL, 0, NULL);
+   SetAssertFailhandle(handle);
+
+   SimpleFsSDeviceWriteFile(handle, 0, firstFileData, firstFileDataSize);
+   SimpleFsSDeviceWriteFile(handle, 1, secondFileData, secondFileDataSize);
+
+   for (uint16_t i = 0; i < 10000; i++)
+   {
+      SimpleFsSDeviceWriteFile(handle, 0, firstFileData, firstFileDataSize);
+      SimpleFsSDeviceWriteFile(handle, 1, secondFileData, secondFileDataSize);
+   }
+
+   size_t sizeOfFirstFile = SimpleFsSDeviceReadFile(handle, 0, firstFileData, 14);
+   size_t sizeOfSecondFile = SimpleFsSDeviceReadFile(handle, 1, secondFileData, 17);
+
+   TEST_ASSERT_EQUAL(14, sizeOfFirstFile);
+   TEST_ASSERT_EQUAL(17, sizeOfSecondFile);
+}
+
+TEST(Transfer, TransferWithRewriteFileAndRandomGenerationBadBlocks)
+{
+   ShowLog(false);
+
+   uint16_t firstFileDataSize = GetTestFileDataSize(FILE_WITH_FULLY_FILLED_DATA_BLOCKS);
+   char firstFileData[firstFileDataSize];
+   CopyTestFileData(firstFileData, FILE_WITH_FULLY_FILLED_DATA_BLOCKS);
+
+   uint16_t secondFileDataSize = GetTestFileDataSize(FILE_WITH_INCOMPLETE_FILLED_LAST_DATA_BLOCK);
+   char secondFileData[secondFileDataSize];
+   CopyTestFileData(secondFileData, FILE_WITH_INCOMPLETE_FILLED_LAST_DATA_BLOCK);
+
+   uint16_t amountOfBadBlocks = 7;
+   uint16_t amountBlocksInSector = 24;
+   uint16_t badBlockNumbers[amountOfBadBlocks];
+
+   srand(time(NULL));
+   for (uint16_t i = 0; i < amountOfBadBlocks; ++i)
+      badBlockNumbers[i] = rand()%amountBlocksInSector;
+
+   SetGlobalBadBlocksNumbersArrayPtr(badBlockNumbers, 1);
+   SetGlobalBadBlocksNumbersArraySize(sizeof(badBlockNumbers), 1);
+   SetGlobalBadBlocksNumbersArrayPtr(badBlockNumbers, 0);
+   SetGlobalBadBlocksNumbersArraySize(sizeof(badBlockNumbers), 0);
+
+   CREATE_SIMPLE_FS_APPLICATION(200, this);
+
+   SIMPLE_FS_DISPOSE_HANDLE_CLEANUP_ATTRIBUTE SDEVICE_HANDLE(SimpleFs) *handle =
+         SDEVICE_CREATE_HANDLE(SimpleFs)(&INIT(this), NULL, 0, NULL);
+   SetAssertFailhandle(handle);
+
+   SimpleFsSDeviceWriteFile(handle, 0, firstFileData, firstFileDataSize);
+   SimpleFsSDeviceWriteFile(handle, 1, secondFileData, secondFileDataSize);
+
+   for (uint16_t i = 0; i < 10000; i++)
+   {
+      for (uint16_t j = 0; j < amountOfBadBlocks; ++j)
+         badBlockNumbers[j] = rand()%amountBlocksInSector;
+
+      SimpleFsSDeviceWriteFile(handle, 0, firstFileData, firstFileDataSize);
+      SimpleFsSDeviceWriteFile(handle, 1, secondFileData, secondFileDataSize);
+   }
+
+   size_t sizeOfFirstFile = SimpleFsSDeviceReadFile(handle, 0, firstFileData, 14);
+   size_t sizeOfSecondFile = SimpleFsSDeviceReadFile(handle, 1, secondFileData, 17);
+
+   TEST_ASSERT_EQUAL(14, sizeOfFirstFile);
+   TEST_ASSERT_EQUAL(17, sizeOfSecondFile);
+}
+
 TEST_GROUP_RUNNER(Transfer)
 {
    RUN_TEST_CASE(Transfer, TransferTwoFiles);
    RUN_TEST_CASE(Transfer, TransferWithRewriteFile);
+   RUN_TEST_CASE(Transfer, TransferWithBadBlocks);
+   RUN_TEST_CASE(Transfer, TransferWithRewriteFileAndBadBlocks);
+   RUN_TEST_CASE(Transfer, TransferWithRewriteFileAndRandomGenerationBadBlocks);
 }
