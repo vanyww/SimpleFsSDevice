58eba38b547468985e0e9d16395d7cffddbb11b7
Avvesey Erockhov <kolyann088@gmail.com>
2023-09-12 00:04:56 +0300

    Remove SDeviceCore version validation from public interface
    now it`s in dependensies.h file

--- Inc/SimpleFsSDevice/public.h
+++ Inc/SimpleFsSDevice/public.h
@@ -21,11 +21,6 @@
 #include "dependensies.h"
 
 #include <stdbool.h>
-
-#if (SDEVICE_CORE_VERSION_MAJOR != 8) || (SDEVICE_CORE_VERSION_MINOR < 0)
-#error SDevice core version is incorrect.
-#endif
-
 #ifdef DOXYGEN
 
 /**
